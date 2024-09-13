89561234183d9807793b905bb3089b628566149f
dgalilove <dgalilove@gmail.com>
2024-09-13 16:59:25 +0300

    Add filtered input and clear button

--- book.controller.js
+++ book.controller.js
@@ -1,47 +1,49 @@
 "use strict"
 
 function onInIt() {
-  _loadBooks()  
-  renderBooks() 
+  _loadBooks()
+  renderBooks(orderByTitle(gBooks))
 }
 
-function renderBooks() {
-	const books = getBooks()
-	var strHTML = ""
-	books.map((book) => {
-		strHTML += `
-            <tr>
-              <td>${book.title}</td>
-              <td>${book.price}</td>
-              <td>
-							<button onclick="onReadBook('${book.id}')">Read</button>
-      				<button onclick="onUpdateBook('${book.id}')">Update</button>
-      				<button onclick="onRemoveBook('${book.id}')">Delete</button></td>
-            </tr>
-    `
-	})
-	var elBooks = document.querySelector("tbody")
-	elBooks.innerHTML = strHTML
+function renderBooks(books = getBooks()) { // if there are books render them , if not render the gBook from the getBooks()
+  const strHTML = books.map(book => 
+		 `
+    <tr>
+      <td>${book.title}</td>
+      <td>${book.price}</td>
+      <td>
+        <button onclick="onReadBook('${book.id}')">Read</button>
+        <button onclick="onUpdateBook('${book.id}')">Update</button>
+        <button onclick="onRemoveBook('${book.id}')">Delete</button>
+      </td>
+    </tr>
+     `).join('')
+	const elTBody = document.querySelector('tbody')
+  elTBody.innerHTML = strHTML
 }
 
 function onRemoveBook(bookId) {
 	removeBook(bookId)
 	_saveBooks()
-	renderBooks()
+	renderBooks(orderByTitle(getBooks()))
 }
 
 function onUpdateBook(bookId) {
 	updatePrice(bookId)
 	_saveBooks()
-	renderBooks()
+	renderBooks(orderByTitle(getBooks()))
 }
 
 function onAddBook() {
 	addBook()
 	_saveBooks()
-	renderBooks()
+	renderBooks(orderByTitle(getBooks()))
 }
 
 function onReadBook(bookId) {
 	readBook(bookId)
+}
+
+function onFilterBooks(input) {
+	filterBooks(input)
 }
--- services/book.service.js
+++ services/book.service.js
@@ -1,86 +1,78 @@
-// Store the original gBooks in a separate array
 const gDemoBooks = [
-  {
-    id: "bg4J78",
-    title: "batman-rebirth",
-    price: 120,
-    imgUrl: "img/batman-rebirth.jpg",
-  },
-  {
-    id: "bh2653",
-    title: "Narnia",
-    price: 150,
-    imgUrl: "img/narnia.jpg",
-  },
-  {
-    id: "ag4L79",
-    title: "Diaries of a wimpy kid",
-    price: 100,
-    imgUrl: "img/Diaries of a wimpy kid.jpg", 
-  }
+	{
+		id: "bg4J78",
+		title: "batman-rebirth",
+		price: 120,
+		imgUrl: "img/batman-rebirth.jpg",
+	},
+	{ id: "bh2653", title: "Narnia", price: 150, imgUrl: "img/narnia.jpg" },
+	{
+		id: "ag4L79",
+		title: "Diaries of a wimpy kid",
+		price: 100,
+		imgUrl: "img/Diaries of a wimpy kid.jpg",
+	},
 ]
 
-var gBooks = [...gDemoBooks]
+var gBooks = [...gDemoBooks] // making a copy
 
-function getBooks(){
-  return gBooks
+function getBooks() { // getting books
+	return gBooks
 }
 
-function removeBook(bookId){
-  const bookIdx = gBooks.findIndex(book => book.id === bookId)
-  if (bookIdx !== -1){
-    gBooks.splice(bookIdx, 1)
-  }
+function removeBook(bookId) { // removing proce
+	const bookIdx = gBooks.findIndex((book) => book.id === bookId)
+	if (bookIdx !== -1) gBooks.splice(bookIdx, 1)
 }
 
-function updatePrice(bookId){
-  const book = gBooks.find(book => book.id === bookId)
-  book.price = +prompt('Update the price')
+function updatePrice(bookId) { // updating price
+	const book = gBooks.find((book) => book.id === bookId)
+	book.price = +prompt("Update the price")
 }
 
-function addBook() {
-  const title = prompt("Enter the title of the book")
-  const price = prompt("Enter the price of the book")
-  var book = {
-    id: makeId(),
-    title: title,
-    price: price,
-    imgUrl: `img/${title}.jpg`,
-  };
-  gBooks.unshift(book)
+function addBook() { // adding book
+	const title = prompt("Enter the title of the book")
+	const price = prompt("Enter the price of the book")
+	const book = { id: makeId(), title, price, imgUrl: `img/${title}.jpg` }
+	gBooks.unshift(book)
 }
 
-function readBook(bookId) {
-  const book = gBooks.find(book => book.id === bookId)
-  const elModal = document.querySelector('.modal')
-  const elImg = document.querySelector('.modal-content')
+function readBook(bookId) { // if id is matching open the modal , show the picture and on click close
+	const book = gBooks.find((book) => book.id === bookId)
+	const elModal = document.querySelector(".modal")
+	const elImg = document.querySelector(".modal-content")
 
-  elModal.style.display = 'block'
-  elImg.src = book.imgUrl
-
-  elModal.onclick = function () {
-    closeModal();
-  };
+	elModal.style.display = "block"
+	elImg.src = book.imgUrl
+	elModal.onclick = closeModal 
 }
 
-function closeModal() {
-  const elModal = document.querySelector('.modal')
-  elModal.style.display = 'none'
+function closeModal() { // closing modal
+	const elModal = document.querySelector(".modal")
+	elModal.style.display = "none"    
 }
 
-function _saveBooks(){
-  saveToStorage('books', gBooks)
+function _saveBooks() {
+	saveToStorage("books", gBooks)  // save to local storage 
 }
 
-function _loadBooks() {
-  var books = loadFromStorage('books')
-  
-  if (books && books.length > 0) {
-    gBooks.length = 0
-    gBooks.push(...books)
-  } else {
-    gBooks.length = 0
-    gBooks.push(...gDemoBooks)
-  }
+function _loadBooks() { // load from local storage
+  const books = loadFromStorage('books')
+  gBooks = books && books.length > 0 ? books : [...gDemoBooks] // if there are books load to gBooks , if there aren't books load the demo 
 }
 
+function orderByTitle(books) { //order by title
+	books.sort((a, b) => a.title.localeCompare(b.title))
+}
+
+function filterBooks(input) { //filter by the title 
+	const filter = getBooks().filter((book) => 
+		book.title.toLowerCase().includes(input.toLowerCase()) // turning the input to lower case then checking if the input included in the book title
+	)
+	renderBooks(filter) // rendering the filtered books 
+}
+
+function clearSearch() {
+	document.querySelector("input").value = "" // clearing the input 
+	renderBooks(gBooks) // rendering the books 
+}
